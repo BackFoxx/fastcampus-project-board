6fad15970f3e2b3e1b09ce1cbfdf43bb0425e8fd
backFox <jorealbusiness@gmail.com>
2022-08-06 13:03:21 +0900

    #22 - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleCommentService.java
@@ -24,4 +24,12 @@ public class ArticleCommentService {
     public void saveArticleComment(ArticleCommentDto dto) {
 
     }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+
+    }
 }
--- src/main/java/com/fastcampus/projectboard/service/ArticleService.java
+++ src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -2,15 +2,13 @@ package com.fastcampus.projectboard.service;
 
 import com.fastcampus.projectboard.domain.type.SearchType;
 import com.fastcampus.projectboard.dto.ArticleDto;
-import com.fastcampus.projectboard.dto.ArticleUpdateDto;
+import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -19,7 +17,7 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
@@ -32,11 +30,16 @@ public class ArticleService {
 
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void deleteArticle(long articleId) {
 
     }
 
-    public void deleteArticle(long articleId) {
+    @Transactional(readOnly = true)
+    public ArticleWithCommentsDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void updateArticle(ArticleDto dto) {
 
     }
 }
